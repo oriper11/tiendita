0f79ecbcb455255753dbe92a266f1470b7871e3b
oriper <orianaperdomo11@gmail.com>
2022-11-14 22:22:01 -0300

    ok

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,7 @@
 import React, { useContext } from "react";
 import cartContext from "../../storage/CartContext";
-//6. Para consumir el context importamos el cartContext y el hook useContext()
 
 function CartWidget() {
-  //7. Invocamos useContext(contexto) y recibimos el VALUE del provider
   const { totalItemsInCart } = useContext(cartContext);
 
   return (
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NewtonsCradle } from "@uiball/loaders";
 
 function Loader(props) {
-  return <NewtonsCradle {...props} />;
+  return <NewtonsCradle {...props} color="violet" />;
 }
 
 export default Loader;
